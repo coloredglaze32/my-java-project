d23f838b971a9d0a75a0b5c0dd0530c73d6a2649
color <1075767612@qq.com>
2025-02-25 15:30:10 +0800

    更新代码

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
 public class Main{
     public static void main(String[] args) {
         System.out.println("hello world");
+        // 首次链接到远程仓库：git remote add origin https://github.com/coloredglaze32/my-java-project.git
+
+        // 推送代码到github
+        /*
+        git branch -M main
+        git push -u origin main
+         */
+
+        //后续更新代码
+        /*
+        git add .
+        git commit -m “更新代码”
+        git push origin main
+         */
     }
 }
